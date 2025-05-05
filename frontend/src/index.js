3ba7a5c4c26dd2dfc8d0395d41c94fad1e15fdf8
Stas Zaitsev <ststas@yandex.ru>
2025-05-05 12:21:54 +0300

    fix: BrowserRouter

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,8 +8,8 @@ import App from './components/App'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRoute basename="/mesto">
+    <BrowserRouter basename="/mesto">
       <App />
-    </BrowserRoute>
+    </BrowserRouter>
   </React.StrictMode>,
 )
